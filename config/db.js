02535b3a4ee0864ea95929d75634a630ee36e3fe
Your Name <you@example.com>
2022-10-27 09:29:36 -0500

    Rutas de usuario

--- config/db.js
+++ config/db.js
@@ -1,31 +1,15 @@
 const sequelize = require('./seq')
 const colors = require ('colors')
-const {DataTypes} = require('sequelize')
-
-//Crear instancia del modelo user 
-const UserModel = require('../models/user')
-const User =  UserModel(sequelize, DataTypes)
-
-//definir la funcion de conexion a la base de datos
-
 
 const connectDB = async ()=>{
     try{
     //conectarse a la base de datos 
     await sequelize.authenticate()
     console.log('Conectado a mysql'.bgBlue.green)
-    const jane = await User.create(
-        { username: "Karol", 
-        email: "karol@misena.edu.co",
-        password: "45678"
-        });
-console.log("Jane's auto-generated ID:", jane.username);
-
 }catch (error){
     console.log(error)
 }
 
-
 } 
 
-connectDB()
+module.exports = connectDB
--- server.js
+++ server.js
@@ -3,36 +3,31 @@ const express = require('express')
 //2. Citar las dependenias necesarias para el proyecto 
 const dotenv = require('dotenv')
 const colors = require('colors')
+const connectDB = require('./config/db')
 const listEndpoint = require('express-list-endpoints')
 //los componentes de ruta
 const bootcampRoutes = require('./routes/BootcampRoutes')
 const coursesRoutes = require('./routes/CoursesRoutes')
+const userRoutes = require('./routes/UserRoutes')
 
 
 //3. establecer archivo de configuracion
 dotenv.config({
     path:'./config/config.env'
 })
-//console.log(process.env.PORT)
-
-
 //crear el objeto aplicacion
 //para el servidor de desarrollo
 const app = express()
+//validemos el objero application para recibir datos en formato json
+app.use(express.json())
+//conexion a bd
+connectDB()
 
 //rutas de proyecto
 app.use('/api/v1/bootcamps', bootcampRoutes)
 app.use('/api/v1/courses', coursesRoutes)
+app.use('/api/v1/users', userRoutes)
 
-//endpoint de aplicacion
-app.get('/' , (request , response )=>{
-    response
-    .status(200)
-    .json({
-        "success": true,
-        "data" : "reqquest exitosa"
-    })
-})
 
 //endpoints de dominio
 //bootcamp
